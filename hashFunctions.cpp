8b4b06393aa78fc65cbe20df8e94da5aef92bc44
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2016-06-14 11:25:35 +0200

    Funciones de hashing añadidas

--- hashFunctions.cpp
+++ hashFunctions.cpp
@@ -1,5 +1,6 @@
 #include "hashFunction"
 
+
 using namespace std;
 
 int hashFunctions::hashFunction( const int i, const int numHashFunction, const int sizeHash ) const
@@ -10,6 +11,13 @@ int hashFunctions::hashFunction( const int i, const int numHashFunction, const i
 			return modulHF( i, sizeHash );
 			break;
 		case 2:
+			return djb2( i );
+			break;
+		case 3:
+			return sdbm( i );
+			break;
+		case 4:
+			return Jenkins( i );
 			break;
 		default:
 			break;
@@ -24,4 +32,34 @@ int hashFunctions::getNumHashFunctions() const
 int hashFunctions::modulHF( const int i, const int hashSize ) const
 {
 	return i%hashSize;
+}
+
+int hashFunctions::djb2( const int i ) const
+{
+	//revisar si --c o c = c >> 3
+    int hash = 5381; 
+    for (int c = i; c > 0; --c) {
+    	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return hash;
+}
+
+int hashFunctions::sdbm( const int i ) const
+{
+	int hash = 0;
+	for (int c = i; c > 0; --c){
+		hash = c + (hash << 6) + (hash << 16) - hash;
+	}
+	return hash;
+}
+
+int hashFunctions::Jenkins ( const int i ) const
+{
+   i = (i+0x7ed55d16) + (i<<12);
+   i = (i^0xc761c23c) ^ (i>>19);
+   i = (i+0x165667b1) + (i<<5);
+   i = (i+0xd3i2646c) ^ (i<<9);
+   i = (i+0xfd7046c5) + (i<<3);
+   i = (i^0xb55i4f09) ^ (i>>16);
+   return a;
 }
--- hashFunctions.h
+++ hashFunctions.h
@@ -10,5 +10,8 @@ class hashFunctions {
 
 	private:
 		int numHashFunctions = 1;
+		int djb2( const int i ) const;
 		int modulHF( const int i, const int hashSize ) const;
+		int sdbm( const int i ) const;
+		int Jenkins ( const int a, const int b, const int i ) const;
 }
