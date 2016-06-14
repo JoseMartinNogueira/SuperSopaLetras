b10df64e5b9bef27f9453f59cafabfbfa7f95af5
RickBinary <arc.binary.hex@gmail.com>
2016-06-15 01:00:21 +0200

    generador Diccionario v1

---
+++ gD.cpp
@@ -0,0 +1,31 @@
+#include "Tools.cpp"
+
+int main(int argc, char *argv[]) {
+    if(argc == 1) {
+        cout << endl;
+        cout << "Uso: >./" << argv[0] << " nombreDiccionario numeroElemetos min max [nombreTablero porcentaje]" << endl;
+        cout << "\t- nombreDiccionario 'para indicar el nombre del fichero a generar'" << endl;
+        cout << "\t- numeroElementos 'para indicar cuantos numeros se tienen que generar" << endl;
+        cout << "\t- min 'los numeros del diccionario seran mayor o igual a este valor" << endl;
+        cout << "\t- max 'los numeros del diccionario seran menores o igual a este valor" << endl;
+        cout << "\t- nombreTablero 'nombre tablero para generar el diccionario" << endl;
+        cout << "\t- porcentaje 'porcentaje aproximado de elementos que perteneceran al tablero" << endl;
+        cout << endl;
+    }
+    else if (argc == 5) {
+        Tools T;
+        T.generarD(argv[1], atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), 0);
+    }
+    else {
+        cout << endl;
+        cout << "!!! MAL USO !!!" << endl;
+        cout << "Uso: >./" << argv[0] << " nombreDiccionario numeroElemetos min max [nombreTablero porcentaje]" << endl;
+        cout << "\t- nombreDiccionario 'para indicar el nombre del fichero a generar'" << endl;
+        cout << "\t- numeroElementos 'para indicar cuantos numeros se tienen que generar" << endl;
+        cout << "\t- min 'los numeros del diccionario seran mayor o igual a este valor" << endl;
+        cout << "\t- max 'los numeros del diccionario seran menores o igual a este valor" << endl;
+        cout << "\t- nombreTablero 'nombre tablero para generar el diccionario" << endl;
+        cout << "\t- porcentaje 'porcentaje aproximado de elementos que perteneceran al tablero" << endl;
+        cout << endl;
+    }
+}
