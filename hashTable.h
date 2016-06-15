97f1be2c65c88398f7a2002124849c6f9b77d269
JoseMartinNogueira <jose.martin.nogueira@est.fib.upc.edu>
2016-06-15 10:53:04 +0200

    A

--- hashTable.h
+++ hashTable.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include "hashFunctions.h"
+#include "Tools.h"
 
 using namespace std;
 
@@ -12,9 +14,8 @@ class hashTable {
 	public:
 		
 		void createHashTable( const vector<int>& diccionario, const int i );
-		void createHashTable( const  matrix& sopaLetras, const int i );
+		void createHashTable( const  matrix& sopaLetras, const int i, const int  );
 
-		bool insert( const int x );
 		bool contains( const int x );
 		hashTable getHashTable() const;
 
